5a6a48b429d946ee3c3b72b9b2ff55bd9dc45534
Hanifalm <156154883+Hanifalm@users.noreply.github.com>
2024-02-13 16:20:38 +0700

    Update banner.js

--- banner.js
+++ banner.js
@@ -1,1 +1,10 @@
-
+<script type="text/javascript">
+	atOptions = {
+		'key' : '2d223343ff6e35f673f85a5c1e4a669d',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//slippersprimeexaltation.com/2d223343ff6e35f673f85a5c1e4a669d/invoke.js"></scr' + 'ipt>');
+</script>
